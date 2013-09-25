aa4ddf591ac0c96a6b5f076191ef25215c2dc21a
NeoAtlantis <rijndael@yeah.net>
2013-09-26 01:47:35 +0800

    using label to display status

--- interface/static/js/index.js
+++ interface/static/js/index.js
@@ -43,6 +43,23 @@ var xmppPanel = function(){
             setTimeout(function(){self.display.refresh();}, 5000);
         },
 
+        _getStatusObject: function(statusText){
+            var c = {
+                'PREAUTH': ['danger', '未登录'],
+                'AUTH': ['default', '认证中'],
+                'AUTHED': ['warning', '已认证'],
+                'BIND': ['info', '登录中'],
+                'SESSION': ['primary', '登录中'],
+                'ONLINE': ['success', '已登录'],
+            }[statusText];
+            if(c == undefined)
+                c = ['danger', '未知程序错误'];
+            return $('<span>')
+                .addClass('label label-' + c[0])
+                .text(c[1])
+            ;
+        },
+
         data: function(data){
             var tbody = $('#xmpp-clients tbody');
             var jids = [];
@@ -55,7 +72,11 @@ var xmppPanel = function(){
                         $('<tr>', {'data-jid': jid})
                             .append(
                                 $('<td>', {name: 'status'})
-                                    .text(news.client.status)
+                                    .append(
+                                        self.display._getStatusObject(
+                                            news.client.status
+                                        )
+                                    )
                             )
                             .append(
                                 $('<td>', {name: 'jid'})
@@ -81,7 +102,10 @@ var xmppPanel = function(){
                                             .addClass('btn btn-success')
                                             .text('登录')
                                             .click(
-                                                self.display.handlers.onControlLogin
+                                                self
+                                                    .display
+                                                    .handlers
+                                                    .onControlLogin
                                             )
                                     )
                                     .append(
@@ -92,13 +116,23 @@ var xmppPanel = function(){
                                             .addClass('btn btn-danger')
                                             .text('退出')
                                             .click(
-                                                self.display.handlers.onControlLogout
+                                                self
+                                                    .display
+                                                    .handlers
+                                                    .onControlLogout
                                             )
                                     )
                             )
                     );
                 } else {
-                    cor.find('[name="status"]').text(news.client.status);
+                    cor.find('[name="status"]')
+                        .empty()
+                        .append(
+                            self.display._getStatusObject(
+                                news.client.status
+                            )
+                        )
+                    ;
                     cor.find('[name="jid"]').text(news.jid);
                 }
             }
