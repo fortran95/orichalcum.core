68a469c95a08468bfcd7a29a1da405002beb2be2
NeoAtlantis <rijndael@yeah.net>
2013-09-26 00:20:32 +0800

    doing login

--- interface/static/js/index.js
+++ interface/static/js/index.js
@@ -1,27 +1,50 @@
 var functions = {
 
-    xmpp_login: function(jid, pwd){
-        $.ajax({
-            type: "POST",
-            url: '../service/xmpp/' + jid + '/login',
-            data: {password: pwd},
-            success: function(e){
-                
-            },
-            dataType: 'text',
-        });
-    },
+    xmpp: {
+        login: function(jid, pwd){
+            $.ajax({
+                type: "POST",
+                url: '../service/xmpp/' + jid + '/login',
+                data: {password: pwd},
+                success: function(e){
+                    
+                },
+                dataType: 'text',
+            });
+        },
 
-    xmpp_logout: function(jid){
-        $.ajax({
-            type: "GET",
-            url: '../service/xmpp/' + jid + '/logout',
-            success: function(e){
-                
-            },
-            dataType: 'text',
-        });
+        logout: function(jid){
+            $.ajax({
+                type: "GET",
+                url: '../service/xmpp/' + jid + '/logout',
+                success: function(e){
+                    
+                },
+                dataType: 'text',
+            });
+        },
     },
 
 };
 
+var xmppPanel = function(){
+    var self = this;
+
+    this.display = {
+        keys: [],
+        refresh: function(info){
+        },
+    };
+
+    this.init = function(){
+        var createForm = $('#xmpp-clients [name="create"]');
+        createForm.find('[name="login"]').click(function(){
+            functions.xmpp.login(
+                createForm.find('[name="jid"]').val(),
+                createForm.find('[name="pwd"]').val()
+            )
+        });
+    };
+
+    $(function(){self.init();});
+}();
